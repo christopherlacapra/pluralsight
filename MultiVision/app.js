495cd70b41d4b11927db69db9a9402378e8c9abb
christopher.lacapra@gmail.com <christopher.lacapra@gmail.com>
2016-09-11 15:40:29 +0200

    Working skeleton

--- MultiVision/app.js
+++ MultiVision/app.js
@@ -4,15 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongo = require("mongoose");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -25,6 +21,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes); 
 app.use('/users', users);
+
+//DB config
+mongo.connect("mongodb://localhost/multivision");
+var db = mongo.connection;
+
+db.on("error",console.error.bind(console, "Connection error"));
+db.once("open",
+    function () {
+        console.log("db opened");
+    });
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
